6e56f2580dda343588d1bd8b1db9e19bb7ea0910
emilyk <emily.g.kruse@gmail.com>
2022-05-08 21:21:43 -0400

    db, express, sequelize setup

---
+++ server.js
@@ -0,0 +1,34 @@
+const path = require("path");
+const express = require("express");
+const session = require("express-session");
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const sequelize = require("./config/connection");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
+
+// create session
+const sess = {
+  secret: process.env.SECRET,
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+
+// turn on routes
+app.use(routes);
+
+// turn on connection to db and server
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log("Now listening"));
+});
