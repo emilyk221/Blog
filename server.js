ad6dcece9011e8721aa2e752bed502965714e74c
emilyk <emily.g.kruse@gmail.com>
2022-05-11 10:07:50 -0400

    homepage layout and routes

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const path = require("path");
 const express = require("express");
 const session = require("express-session");
 const routes = require("./controllers");
+const exphbs = require("express-handlebars");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -25,6 +26,12 @@ app.use(session(sess));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
+
+const hbs = exphbs.create({});
+
+// set up handlebars
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
 
 // turn on routes
 app.use(routes);
